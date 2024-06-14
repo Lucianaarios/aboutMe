231d926964b106a0001d4e563771d86369a54b5e
Lucianaarios <lucianarios20.lr@gmail.com>
2024-06-14 14:26:31 -0300

    terminado

--- about-me/src/Screens/Contact.js
+++ about-me/src/Screens/Contact.js
@@ -1,29 +1,79 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from './Contact.module.css';
 import { SlSocialLinkedin } from "react-icons/sl";
 import { CgMail } from "react-icons/cg";
 import { FaGithub } from "react-icons/fa";
 
 const Contact = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const formRef = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = formRef.current;
+    if (form) {
+      fetch(form.action, {
+        method: form.method,
+        body: new FormData(form),
+      }).then(() => {
+        setSubmitted(true);
+        form.reset(); 
+      }).catch(error => console.error('Error:', error));
+    }
+  };
+
+  useEffect(() => {
+    if (submitted) {
+      const timer = setTimeout(() => {
+        setSubmitted(false);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [submitted]);
+
   return (
     <div name="Contact" className={styles.contact}>
       <h2>Contact</h2>
-      <form className={styles.form}>
-        <label for="Name">Name</label>
-        <input id="Name" name="Name" className={styles.input}></input>
-        <label for="Email">Email</label>
-        <input id="Email" name="Email" className={styles.input}></input>
-        <label>Message</label>
-        <textarea></textarea>
-        <button type="submit">Enviar</button>
-      </form>
+      {!submitted ? (
+        <form
+          className={styles.form}
+          method="POST"
+          action="https://getform.io/f/pbgxenea"
+          ref={formRef}
+          onSubmit={handleSubmit}
+        >
+          <label htmlFor="Name">Name</label>
+          <input id="Name" name="Name" className={styles.input} required />
+          <label htmlFor="Email">Email</label>
+          <input
+            id="Email"
+            name="Email"
+            type="Email"
+            className={styles.input}
+            required
+          />
+          <label htmlFor="Message">Message</label>
+          <textarea id="Message" name="Message" className={styles.input} required></textarea>
+          <button type="submit">Enviar</button>
+        </form>
+      ) : (
+        <div className={styles.thankYouMessage}>
+          <p>El mensaje se ha enviado con exito</p>
+        </div>
+      )}
       <div className={styles.contactInfo}>
-        <div className={styles.contactItem}><SlSocialLinkedin /> Luciana Rios</div>
-        <div className={styles.contactItem}><CgMail /> lucianarios20.lr@gmail.com</div>
-        <div className={styles.contactItem}><FaGithub /> Lucianaarios</div>
+        <div className={styles.contactItem}>
+          <SlSocialLinkedin /> Luciana Rios
+        </div>
+        <div className={styles.contactItem}>
+          <CgMail /> lucianarios20.lr@gmail.com
+        </div>
+        <div className={styles.contactItem}>
+          <FaGithub /> Lucianaarios
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
